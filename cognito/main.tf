66736ea22b0ad1ba959639f76134ed65a1650e20
Uzama Zaid <uzama.jaward@system73.com>
2023-05-20 12:55:56 +0530

    implemented cognito module for user authentication

---
+++ cognito/main.tf
@@ -0,0 +1,77 @@
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired region
+}
+
+resource "aws_cognito_user_pool" "pool" {
+  name                       = var.name
+  email_verification_subject = "Reset password code"
+  email_verification_message = "Your reset password validation code is: {####}"
+  username_attributes        = ["email"]
+  auto_verified_attributes   = ["email"]
+
+  admin_create_user_config {
+    allow_admin_create_user_only = false
+  }
+
+  email_configuration {
+    reply_to_email_address = "" 
+  }
+
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+  }
+
+  schema {
+    attribute_data_type      = "String"
+    name                     = "email"
+    required                 = true
+    developer_only_attribute = false
+    mutable                  = true
+
+    string_attribute_constraints {
+      min_length = "7"
+      max_length = "100"
+    }
+  }
+
+  password_policy {
+    minimum_length    = 8
+    require_numbers   = true
+    require_lowercase = true
+    require_symbols   = false
+    require_uppercase = true
+  }
+
+#   lambda_config {
+#     pre_sign_up = var.auto_confirm_user_lambda
+#   }
+
+  tags = {
+    Name = var.name
+  }
+
+  user_pool_add_ons {
+    advanced_security_mode = "OFF"
+  }
+
+  mfa_configuration = "OPTIONAL"
+
+  software_token_mfa_configuration {
+    enabled = false
+  }
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name                = var.name
+  user_pool_id        = aws_cognito_user_pool.pool.id
+  explicit_auth_flows = ["ADMIN_NO_SRP_AUTH", "USER_PASSWORD_AUTH"]
+  generate_secret     = false
+}
+
+resource "aws_cognito_user_group" "admin" {
+  name         = "admin"
+  user_pool_id = aws_cognito_user_pool.pool.id
+}
---
+++ cognito/variables.tf
@@ -0,0 +1,4 @@
+variable "name" {
+  type        = string
+  default     = "restaurant-manager"
+}
