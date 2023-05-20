43d3461e69ff5b29bb5a72cd50f8265ae531b8b4
Uzama Zaid <uzama.jaward@system73.com>
2023-05-20 18:23:18 +0530

    database schema updated

--- database/database.sql
+++ database/database.sql
@@ -1,42 +1,32 @@
 use restaurant;
 
-CREATE TABLE IF NOT EXISTS users (
-  id INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password_hash VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE IF NOT EXISTS restaurants (
-  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL,
-  address VARCHAR(255) NOT NULL,
-  phone VARCHAR(20) NOT NULL,
-  user_id INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  address VARCHAR(255) NULL,
+  phone VARCHAR(20) NULL,
+  PRIMARY KEY (username)
 );
 
 CREATE TABLE IF NOT EXISTS menu_items (
   id VARCHAR(255) NOT NULL,
-  restaurant_id INT NOT NULL,
+  restaurant_username VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL,
   description VARCHAR(255),
   price DECIMAL(10,2) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (restaurant_id) REFERENCES restaurants(id)
+  FOREIGN KEY (restaurant_username) REFERENCES restaurants(username)
 );
 
 CREATE TABLE IF NOT EXISTS orders (
   id VARCHAR(255) NOT NULL,
-  restaurant_id INT NOT NULL,
+  restaurant_username VARCHAR(255) NOT NULL,
   customer_name VARCHAR(255) NOT NULL,
   customer_phone VARCHAR(20) NOT NULL,
   status ENUM('received', 'preparing', 'out_for_delivery', 'delivered') NOT NULL,
   order_date DATETIME NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (restaurant_id) REFERENCES restaurants(id)
+  FOREIGN KEY (restaurant_username) REFERENCES restaurants(username)
 );
 
 CREATE TABLE IF NOT EXISTS order_items (
