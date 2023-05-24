85542d396441b1a9470581f7a3902970a9336a3b
Uzama Zaid <uzama.jaward@system73.com>
2023-05-24 14:34:46 +0530

    implemented reolver and mapping templates for listRestaurant query

--- appsync/local.tf
+++ appsync/local.tf
@@ -49,3 +49,11 @@ data "local_file" "pipeline_before_request" {
 data "local_file" "pipeline_after_request" {
   filename = "${path.module}/templates/pipeline_after_request.vtl"
 }
+
+data "local_file" "list_restaurant_request" {
+  filename = "${path.module}/templates/list_restaurant_request.vtl"
+}
+
+data "local_file" "list_restaurant_response" {
+  filename = "${path.module}/templates/list_restaurant_response.vtl"
+}
--- appsync/resolvers.tf
+++ appsync/resolvers.tf
@@ -47,3 +47,14 @@ resource "aws_appsync_resolver" "create_restaurant" {
     ]
   }
 }
+
+resource "aws_appsync_resolver" "list_restaurant" {
+  provider          = aws.useast
+  api_id            = aws_appsync_graphql_api.appsync_api.id
+  data_source       = "AuroraRDS"
+  depends_on        = [null_resource.appsync_rds_datasource]
+  field             = "listRestaurant"
+  request_template  = data.local_file.list_restaurant_request.content
+  response_template = data.local_file.list_restaurant_response.content
+  type              = "Query"
+}
